84c0846cf9df6d2972e82e4638f6bca1e4d3f1cb
Attilius <leo20@freemail.hu>
2021-08-14 22:47:47 +0200

    I added a new function called calculatePercent

--- functions.php
+++ functions.php
@@ -7,3 +7,13 @@ function saveVotes($dayOfYear, $data)
     fwrite($fp, json_encode($data));
     fclose($fp);
 }
+
+function calculatePercent($totalVotes, $votesOfItem) 
+{
+    // 0-val való osztás elkerülése
+    if ($totalVotes < 1) {
+        return 0;
+    }
+    $result = $votesOfItem * 100  / $totalVotes;
+    return round($result);
+}
