90e6128833b73167b7a5e88550f449a7cd0447d3
Attilius <leo20@freemail.hu>
2021-08-15 12:37:46 +0200

    I added a new function called setRankColor

--- functions.php
+++ functions.php
@@ -17,3 +17,52 @@ function calculatePercent($totalVotes, $votesOfItem)
     $result = $votesOfItem * 100  / $totalVotes;
     return round($result);
 }
+
+function setRankColor($totalVotes, $votesOfItem) {
+    $percent = calculatePercent($totalVotes, $votesOfItem);
+    switch ($percent) {
+        case ($percent < 10):
+            return "var(--rank10)";
+            break;
+        
+        case ($percent >= 10 && $percent < 20):
+            return "var(--rank9)";
+            break;
+        
+        case ($percent >= 20 && $percent < 30):
+            return "var(--rank8)";
+            break;
+        
+        case ($percent >= 30 && $percent < 40):
+            return "var(--rank7)";
+            break;
+        
+        case ($percent >= 40 && $percent < 50):
+            return "var(--rank6)";
+            break;
+        
+        case ($percent >= 50 && $percent < 60):
+            return "var(--rank5)";
+            break;
+        
+        case ($percent >= 60 && $percent < 70):
+            return "var(--rank4)";
+            break;
+        
+        case ($percent >= 70 && $percent < 80):
+            return "var(--rank3)";
+            break;
+        
+        case ($percent >= 80 && $percent < 90):
+            return "var(--rank2)";
+            break;
+        
+        case ($percent >= 90):
+            return "var(--rank1)";
+            break;
+        
+        default:
+            var_dump('Error: Wrong rank or color!');
+            break;
+    }
+}
