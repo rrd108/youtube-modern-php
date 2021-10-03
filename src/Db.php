24bc0e2afc129d85e3dba32b938560c4064ec915
rrd <rrd@1108.cc>
2021-10-03 16:58:17 +0200

    method refactored

--- src/Db.php
+++ src/Db.php
@@ -30,9 +30,9 @@ class Db
 
     public function saveNewAnswer(int $question_id, string $answer) {
         $stmt = $this->pdo->prepare('INSERT INTO answers (question_id, answer, votes) VALUES (?, ?, ?)');
+        $stmt = $this->pdo->prepare('INSERT INTO answers (question_id, answer, votes) VALUES (?, ?, 1)');
         $stmt->bindValue(1, $question_id);
         $stmt->bindValue(2, $answer);
-        $stmt->bindValue(3, 1);
         $stmt->execute();
     }
 
