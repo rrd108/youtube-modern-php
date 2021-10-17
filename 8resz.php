29bfec70f2a6020b427031c1bcdf536c1863655c
rrd <rrd@1108.cc>
2021-10-17 16:27:25 +0200

    shorthand ternary

--- 8resz.php
+++ 8resz.php
@@ -1,5 +1,7 @@
 <?php
+error_reporting(E_ALL);
 
+$name = 'Gauranga';
 // ternary operator
 /*if ($name == 'rrd') {
     $channel = 'webmania';
@@ -8,3 +10,6 @@
 }*/
 
 $channel = ($name == 'rrd') ? 'webmania' : null;
+
+//if (isset($_GET['search'])) {...}
+echo $search = $_GET['search'] ?: 'all';       // ha "igazi" / truely - shorthand ternary 
