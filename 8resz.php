1c5fe95c13a86b2cb57d8c73de8a6cb6da3910b3
rrd <rrd@1108.cc>
2021-10-17 17:42:15 +0200

    optional chaning / null safe operator

--- 8resz.php
+++ 8resz.php
@@ -15,3 +15,24 @@ $channel = ($name == 'rrd') ? 'webmania' : null;
 echo $search = $_GET['search'] ?: 'all';       // ha "igazi" / truely - shorthand ternary 
 
 echo $search = $_GET['search'] ?? 'all';       // ha létezik - null coalescing 
+
+// optional chaning / null safe operator
+$users = [
+    [
+        'name' => 'rrd',
+        'stack' => [
+            'frontend' => 'js'
+        ]
+    ],
+    [
+        'name' => 'lame',
+        'stack' => null
+    ]
+];
+$users = json_decode(json_encode($users));
+
+echo "\n\n";
+foreach ($users as $user) {
+    //echo $user->name . ': ' . $user->stack ? $user->stack->frontend : null . "\n";
+    echo $user->name . ': ' . $user->stack?->frontend . "\n";
+}
